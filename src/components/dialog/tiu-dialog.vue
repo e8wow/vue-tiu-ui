d413bf327d944ff96b6142d405f7171549dd0d8c
jelf <okxiaoliang4@gmail.com>
2017-10-13 11:36:25 +0800

    feat(*): 新增dialog组件

---
+++ src/components/dialog/tiu-dialog.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="tiu-t-dialog">
+        <transition :name="maskTransition">
+            <div class="tiu-mask" @click="hide" v-show="show"></div>
+        </transition>
+        <transition :name="dialogTransition">
+            <div :class="dialogClass" v-show="show" :style="dialogStyle">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tiu-dialog',
+    model: {
+        prop: 'show',
+        event: 'change'
+    },
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        },
+        maskTransition: {
+            type: String,
+            default: 'tiu-mask'
+        },
+        dialogTransition: {
+            type: String,
+            default: 'tiu-dialog'
+        },
+        dialogClass: {
+            type: String,
+            default: 'tiu-dialog'
+        },
+        dialogStyle: Object,
+        hideOnBlur: {
+            type: Boolean,
+            default: true
+        }
+    },
+    watch: {
+        show (val) {
+            this.$emit('update:show', val)
+            this.$emit(val ? 'on-show' : 'on-hide')
+        }
+    },
+    methods: {
+        hide () {
+            if (this.hideOnBlur) {
+                this.$emit('update:show', false)
+                this.$emit('change', false)
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "../../styles/transition";
+    @import "../../styles/tips/tiu-mask";
+    @import "../../styles/tips/tiu-dialog";
+</style>
---
+++ src/demo/dialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="show = !show">测试</button>
+        <tiu-dialog :show="show" @change="change">
+            <div>123</div>
+        </tiu-dialog>
+    </div>
+</template>
+
+<script>
+import TiuDialog from '../components/dialog/tiu-dialog.vue'
+
+export default {
+    components: {TiuDialog},
+    data () {
+        return {
+            show: false
+        }
+    },
+    methods: {
+        change (show) {
+            this.show = show
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
